5df8c2bdbc1583f423ce9b26e326762b39218c15
Thach Hoang <thachhoang@users.noreply.github.com>
2013-03-17 16:05:12 -0400

    A shape for each drip?
    How about a shape for each column?

--- shapes.js
+++ shapes.js
@@ -1,4 +1,4 @@
-// Modified from code by Simon Sarris
+﻿// Modified from code by Simon Sarris
 // www.simonsarris.com
 // sarris@acm.org
 //
@@ -10,14 +10,14 @@
 // Constructor for Shape objects to hold data for all drawn objects.
 // For now they will just be defined as rectangles.
 
-function Shape(x, y, w, h, fill) {
+function Shape(x, y, w, h, c, fill) {
 	// This is a very simple and unsafe constructor. All we're doing is checking if the values exist.
 	// "x || 0" just means "if there is a value for x, use that. Otherwise use 0."
 	this.x = x || 0;
 	this.y = y || 0;
 	this.w = w || 1;
 	this.h = h || 1;
-	this.c = 20;
+	this.c = c || 20;
 	this.fill = fill || '#AAAAAA';
 }
 
@@ -52,9 +52,10 @@ function CanvasState(canvas) {
 	this.height = canvas.height;
 	this.ctx = canvas.getContext('2d');
 	
-	this.speed = 5;
+	this.speed = 10;
 	this.barWidth = 20;
 	this.barHeight = 50;
+	this.barCurve = 20;
 	
 	// **** Keep track of state! ****
 	
@@ -85,11 +86,27 @@ CanvasState.prototype.clear = function() {
 CanvasState.prototype.generate = function() {
 	this.shapes = [];
 	
+	var bw = this.barWidth,
+		bh = this.barHeight,
+		bc = this.barCurve,
+		cols = 1 + this.width / bw,
+		rows = 1 + this.height / bh,
+		color;
+	
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+			this.addShape(new Shape(i * bw, j * bh - 20, bw, bh, bc, color));
+		}
+	}
+	
+	/*
 	this.addShape(new Shape(40,40,50,50)); // The default is gray
 	this.addShape(new Shape(60,140,40,60, 'lightskyblue'));
 	// Lets make some partially transparent
 	this.addShape(new Shape(80,150,60,30, 'rgba(127, 255, 212, .5)'));
 	this.addShape(new Shape(125,80,30,80, 'rgba(245, 222, 179, .7)'));	
+	*/
 }
 
 // While draw is called as often as the INTERVAL variable demands,
@@ -107,6 +124,10 @@ CanvasState.prototype.draw = function() {
 		
 		var shapes = this.shapes;
 		
+		var bh = this.barHeight,
+			rows = this.height / bh,
+			rowsH = (bh * (rows + 1)) - 20;
+		
 		// ** Add stuff you want drawn in the background all the time here **
 		
 		// draw all shapes
@@ -114,8 +135,8 @@ CanvasState.prototype.draw = function() {
 		for (var i = 0; i < l; i++) {
 			var shape = shapes[i];
 			shape.y += this.speed;
-			if (shape.y > this.height) {
-				shape.y = -shape.h;
+			if (shape.y > rowsH) {
+				shape.y = -shape.h + 20;
 			}
 			// We can skip the drawing of elements that have moved off the screen:
 			if (shape.x > this.width || shape.x + shape.w < 0) {
@@ -124,6 +145,7 @@ CanvasState.prototype.draw = function() {
 			shapes[i].draw(ctx);
 		}
 		
+		//this.active = false;
 		// ** Add stuff you want drawn on top all the time here **
 	}
 }
@@ -134,7 +156,7 @@ CanvasState.prototype.draw = function() {
 
 function init() {
 	var s = new CanvasState(document.getElementById('canvas1'));
-	s.generate();
+	//s.generate();
 }
 
 // Now go make something amazing!
